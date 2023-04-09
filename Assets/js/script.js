8b9bd0e6ae11b515149fc80953db8f8f991d0058
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-04-09 01:04:01 -0400

    Added ball button for fun

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -54,3 +54,77 @@ randomBtn.addEventListener('mouseenter', () => {
   randomBtn.style.setProperty('--color-g', Math.floor(Math.random() * 255));
   randomBtn.style.setProperty('--color-b', Math.floor(Math.random() * 255));
 })
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+// Set the canvas width and height
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Define the array of balls
+const balls = [];
+
+// Define the Ball class
+class Ball {
+  constructor(x, y, vx, vy, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+    this.radius = radius;
+    this.color = color;
+  }
+
+  // Draw the ball
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+
+  // Update the ball's position
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
+
+    // Check for collisions with the canvas edges
+    if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+      this.vx = -this.vx;
+    }
+
+    if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+      this.vy = -this.vy;
+    }
+  }
+}
+
+// Define the animation loop
+function animate() {
+  // Clear the canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Loop through the balls array and update each ball's position
+  balls.forEach(ball => {
+    ball.draw();
+    ball.update();
+  });
+
+  // Request the next animation frame
+  requestAnimationFrame(animate);
+}
+
+// Add a new ball to the array each time the button is clicked
+document.getElementById('getRandom').addEventListener('click', () => {
+  const x = Math.random() * canvas.width;
+  const y = Math.random() * canvas.height;
+  const vx = Math.random() * 10 - 5;
+  const vy = Math.random() * 10 - 5;
+  const radius = Math.random() * 50 + 10;
+  const color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+  balls.push(new Ball(x, y, vx, vy, radius, color));
+});
+
+// Start the animation loop
+animate();
