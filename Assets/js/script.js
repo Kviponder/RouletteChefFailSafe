11fa26bb9cc706fc5a7b1c1cf3ef1ef4998d3efe
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-04-08 23:13:08 -0400

    THE COMMIT

---
+++ Assets/js/script.js
@@ -0,0 +1,49 @@
+
+const randomRecipeBtn = document.getElementById('getRandom');
+const randomRecipeContainer = document.getElementById('randomRecipe');
+
+function createRandomRecipe(meal) {
+const {strMeal, strMealThumb, strInstructions} = meal;
+//Generate the elements that will apear on the card
+const recipeCard = document.createElement('div');
+  recipeCard.classList.add('recipeCard');
+
+  const recipeCardImg = document.createElement('div');
+  recipeCardImg.classList.add('recipeCard-img');
+
+  const img = document.createElement('img');
+  img.src = strMealThumb;
+  img.alt = strMeal;
+
+  const recipeCardDetails = document.createElement('div');
+  recipeCardDetails.classList.add('recipeCard-details');
+
+  const h2 = document.createElement('h2');
+  h2.textContent = strMeal;
+
+  const instructions = document.createElement('p');
+  instructions.textContent = strInstructions;
+//Assembles HTML
+recipeCardImg.appendChild(img);
+recipeCardDetails.appendChild(h2);
+recipeCardDetails.appendChild(instructions);
+recipeCard.appendChild(recipeCardImg);
+recipeCard.appendChild(recipeCardDetails);
+
+//Clear eexisting recipe details
+randomRecipeContainer.innerHTML = '';
+
+//Add card to page
+randomRecipeContainer.appendChild(recipeCard);
+}
+randomRecipeBtn.addEventListener('click', () => {
+	fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+		.then(response => response.json())
+		.then(response => {
+			createRandomRecipe(response.meals[0]);
+		})
+		.catch(e => {
+			console.warn(e);
+		});
+});
+
---
+++ README.md
@@ -0,0 +1,38 @@
+# Roulette Chef
+
+    Roulette Chef is a web application that provides users with recipe suggestions based on the ingredients they have on hand. It allows users to input an ingredient and get a list of recipe suggestions that feature that ingredient. Additionally, the app provides a "random recipe" button that generates a random recipe suggestion.
+
+## Technologies Used
+
+    HTML, CSS, and JavaScript for the front-end
+
+   MealDB API for recipe data
+
+    Google Analytics API for tracking user behavior
+
+##  Installation
+
+    Clone the repository to your local machine.
+
+    Open the index.html file in a web browser.
+
+
+## Usage
+
+
+    Type an ingredient into the input field and press the "search" button to get recipe suggestions.
+
+    Press the "random recipe" button to generate a random recipe suggestion.
+
+
+## Links
+
+    Live Site: https://kviponder.github.io/team-project/?
+    Repository: https://github.com/Kviponder/team-project
+## User Story
+
+    Roulette Chef is useful for anyone who wants to cook a meal but isn't sure what to make or doesn't want to go to the grocery store. It's also great for people who are trying to use up ingredients they have on hand before they go bad. By simply inputting an ingredient, Roulette Chef provides users with recipe suggestions that are easy to make and use the ingredients they have available.
+
+## screenshots
+  ![alt text](image_url)
+  ![ALT TEXT](image_url)
